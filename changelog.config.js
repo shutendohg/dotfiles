49ab4aa97b2bbfe1b3719a93a92332f1af8c0d80
shutendohg <shutendohg@gmail.com>
2025-07-06 15:38:00 +0000

    chore: Add changelog.config.js again

--- changelog.config.js
+++ changelog.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    disableEmoji: false,
-    format: '{type}: {subject}',
+    disableEmoji: true,
+    format: '{type}: {emoji}{subject}',
     list: ['test', 'feat', 'fix', 'chore', 'docs', 'refactor', 'style', 'ci', 'perf'],
     maxMessageLength: 64,
     minMessageLength: 3,
