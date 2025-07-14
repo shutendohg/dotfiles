610a64f2870b66385df8468062767ab1cfb2ffd3
shutendohg <shutendohg@gmail.com>
2025-07-14 16:14:17 +0000

    chore: Update apt list

--- run_once_00_install-packages.sh
+++ run_once_00_install-packages.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y
 sudo apt-get install -y \
 	neovim fish peco ripgrep fd-find \
 	build-essential libssl-dev curl zip unzip golang-go go-md2man \
-	ca-certificates gnupg lsb-release
+	ca-certificates gnupg lsb-release tree cmake ninja-build luarocks
 
 # ---------- Go & ghq ----------
 export GOPATH="$HOME/go"
