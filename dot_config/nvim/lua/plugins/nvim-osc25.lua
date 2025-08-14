73481bfb8cb9efdcc59bb5147b3781693924ef0a
shutendohg <shutendohg@gmail.com>
2025-08-14 18:21:52 +0000

    style: use linter

--- dot_config/nvim/lua/plugins/nvim-osc25.lua
+++ dot_config/nvim/lua/plugins/nvim-osc25.lua
@@ -1,13 +1,13 @@
 local function setup_osc52()
-    local osc52 = require('osc52')
+    local osc52 = require("osc52")
 
-    vim.keymap.set('n', '<leader>c', osc52.copy_operator, { expr = true })
-    vim.keymap.set('n', '<leader>cc', '<leader>c_', { remap = true })
-    vim.keymap.set('v', '<leader>c', osc52.copy_visual)
-    osc52.setup {
+    vim.keymap.set("n", "<leader>c", osc52.copy_operator, { expr = true })
+    vim.keymap.set("n", "<leader>cc", "<leader>c_", { remap = true })
+    vim.keymap.set("v", "<leader>c", osc52.copy_visual)
+    osc52.setup({
         max_length = 0, -- Unlimited
         silent = false, -- Not display successful messages setting true
-    }
+    })
 end
 
 return {
