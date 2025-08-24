cc7d41c82ad21273ca9bf2aace27a0af627b88e5
shutendohg <shutendohg@gmail.com>
2025-08-24 16:20:34 +0000

    ci: remove installing tmux plugins on dotfiles

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 bash -c "$(curl -fsLS get.chezmoi.io)" -- \
   init --apply git@github.com:shutendohg/dotfiles.git
 ```
+## Install tmux plugins
+This dotfiles doesn't install tmux plugins automaticaclly.
+`Prefix + I` to intall tmux plugins after Quick Install.
--- run_once_30_install-tmux-plugins.sh
+++ run_once_30_install-tmux-plugins.sh
@@ -1,10 +1,15 @@
-#! /usr/bin/env bash
+# ~/.local/share/chezmoi/run_once_30_install-tmux-plugins.sh
+#!/usr/bin/env bash
 set -euo pipefail
 
-TPM_DIR="$HOME/.tmux/plugins/tpm"
-
-if [ ! -d "$TPM_DIR" ]; then
-	git clone --depth=1 https://github.com/tmux-plugins/tpm "$TPM_DIR"
+# Skip on CI
+if [ -n "${CI:-}" ]; then
+    exit 0
 fi
 
-"$TPM_DIR/bin/install_plugins"
+TPM_DIR="$HOME/.tmux/plugins/tpm"
+if [ ! -d "$TPM_DIR" ]; then
+    git clone --depth=1 https://github.com/tmux-plugins/tpm "$TPM_DIR"
+fi
+
+# Not use install_plugins(need to prefix + I)
