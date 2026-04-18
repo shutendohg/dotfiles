24a91c19265610a355eb7594c9c1e92ac7ba7982
shutendohg <shutendohg@gmail.com>
2026-04-18 14:32:45 +0000

    style: formatted the lua file

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -1,10 +1,10 @@
 return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	event = "VeryLazy",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function()
-		vim.opt.termguicolors = true
-		require("bufferline").setup({})
-	end,
+    "akinsho/bufferline.nvim",
+    version = "*",
+    event = "VeryLazy",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+        vim.opt.termguicolors = true
+        require("bufferline").setup({})
+    end,
 }
