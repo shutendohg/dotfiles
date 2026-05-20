e6412c1f2cb95e674c60997f253407a46f17b862
shutendohg <shutendohg@gmail.com>
2026-05-20 15:33:05 +0000

    fix: Update remote clipboard and transter tools

--- dot_config/nvim/lua/config/clipboard.lua
+++ dot_config/nvim/lua/config/clipboard.lua
@@ -1,24 +1,5 @@
-local osc52 = require("vim.ui.clipboard.osc52")
+vim.opt.clipboard = ""
 
-local function paste_from_register(reg)
-	return function()
-		return {
-			vim.fn.split(vim.fn.getreg(reg), "\n"),
-			vim.fn.getregtype(reg),
-		}
-	end
-end
-
-vim.g.clipboard = {
-	name = "OSC 52",
-	copy = {
-		["+"] = osc52.copy("+"),
-		["*"] = osc52.copy("*"),
-	},
-	paste = {
-		["+"] = paste_from_register("+"),
-		["*"] = paste_from_register("*"),
-	},
-}
-
-vim.opt.clipboard = "unnamedplus"
+vim.keymap.set("n", "<leader>y", function()
+    require("vim.ui.clipboard.osc52").copy("+")(vim.fn.getreg('"', 1, true), vim.fn.getregtype('"'))
+end, { desc = "Copy unnamed register via OSC52" })
